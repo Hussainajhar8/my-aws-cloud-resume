dd256d436437fbf125714c3b14110978d27028fa
Miskeen <hussainajhar8@gmail.com>
2023-08-09 19:50:24 +0100

    created terraform files for lambda function and changed function url in index.js

--- iac/lambda/counter_api.py
+++ iac/lambda/counter_api.py
@@ -5,6 +5,7 @@ dynamodb = boto3.resource('dynamodb')
 
 table = dynamodb.Table('cloud_resume_counter_table')
 
+
 def lambda_handler(event, context):
     response = table.get_item(Key={
         'id': '1'
@@ -13,8 +14,8 @@ def lambda_handler(event, context):
     views += 1
     print(views)
     response = table.put_item(Item={
-            'id':'1',
-            'views' : views
+        'id': '1',
+        'views': views
     })
 
     return views
