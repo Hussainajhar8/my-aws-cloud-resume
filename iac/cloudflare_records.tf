1f8df4d261f60362b76262cf609181fc94751e9b
Miskeen <hussainajhar8@gmail.com>
2023-08-21 18:18:25 +0100

    made records idempotent

--- iac/cloudflare_records.tf
+++ iac/cloudflare_records.tf
@@ -13,6 +13,7 @@ module "zone" {
       type  = option.resource_record_type
       value = option.resource_record_value
       ttl   = 3600
+      count = var.existing_records[option.resource_record_name] ? 0 : 1
     }
   ]
 }
